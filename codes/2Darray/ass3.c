0988c8b570c695f1063e050d8689a80b0bc93c6b
ShivSoni5 <shivsonic05@gmail.com>
2018-12-13 00:38:14 +0530

    assignment 3

---
+++ codes/2Darray/ass3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define r 3
+#define c 3
+
+int main() {
+	int m[r][c],i,j,k;
+	for (i=0;i<r;i++){
+		for (j=0;j<c;j++){
+			printf("Enter %d row %d element: ",i+1,j+1);
+			scanf("%d",&m[i][j]);
+		}
+	}
+	for (i=0;i<r;i++){
+		for (k=0;k<i;k++){
+			printf("\t");
+		}
+		for(j=i;j<c;j++){
+			if(i==j)
+			printf("%d\t",m[i][j]);
+		}
+		printf("\n");
+	}
+}
