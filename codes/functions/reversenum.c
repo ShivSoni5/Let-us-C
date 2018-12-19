e464ba8fb919f6524db70c1224edf37e9d68096b
ShivSoni5 <shivsonic05@gmail.com>
2018-12-19 23:42:53 +0530

    reverse using function

--- codes/functions/reversenum.c
+++ codes/functions/reversenum.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+int reverse(int n){
+	int a=0;
+	while(n!=0){
+		a = a*10 + n%10;
+		n /= 10;
+	}
+	return a;
+}
+
+
 int main() {
 	int n;
 	printf("Enter the number: ");
 	scanf("%d",&n);
 	printf("Reverse of the number: ");
-	while(n != 0) {
+	/*while(n != 0) {
 		printf("%d",n%10);
 		n /= 10;
 	}
-	printf("\n");
+	printf("\n");*/
+	printf("%d\n",reverse(n));
 }
