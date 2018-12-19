8bbcc3534b35d2d8ee13c4a6eed57c17c8edd351
ShivSoni5 <shivsonic05@gmail.com>
2018-12-19 23:30:33 +0530

    \n

--- codes/functions/reversenum.c
+++ codes/functions/reversenum.c
@@ -4,9 +4,10 @@ int main() {
 	int n;
 	printf("Enter the number: ");
 	scanf("%d",&n);
-	printf("Reverse of the number is: ");
+	printf("Reverse of the number: ");
 	while(n != 0) {
 		printf("%d",n%10);
 		n /= 10;
 	}
+	printf("\n");
 }
