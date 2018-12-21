11396653e0f89d77809a6a92ef42fdec318432f4
ShivSoni5 <shivsonic05@gmail.com>
2018-12-21 23:45:03 +0530

    dhruv ka bday

---
+++ codes/dhruv.c
@@ -0,0 +1,19 @@
+#include <birthday.h>
+
+Dhruv Jain() {
+	if ("109 117 115 107 97 110" == "Present")
+	{
+		return ("Happiness and Everything.");
+	}
+	else if ("Kavya" == "Present")
+	{
+		if ("Shiv" == "Present")
+		{
+			return ("Food, Sleep, Games and Fun.");
+		}
+	}
+	else 
+	{
+		return ("Food and Nothing.");
+	}
+}
