585d3625ecf09795e0fb8576cf43081ab05f56b5
ShivSoni5 <shivsonic05@gmail.com>
2019-01-07 16:12:04 +0530

    difference in decalaration of preprocessors

--- codes/preprocessors/63.c
+++ codes/preprocessors/63.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define SQR(x) (x*x)
+#define SQR(x) ((x)*(x))
 
 int main()
 {
 	int a, b=3;
 	a = SQR(b+2);
-	printf("\n%d",a);
-}
+	printf("\n%d",a);  // print 11 if SQR(x) (x * x)
+}			   //             --------------
